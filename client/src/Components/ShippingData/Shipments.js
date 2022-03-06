c3db0c6971802eb373bc68d0f245aeaa3b6241b7
Bruno Austin <brunoaugustine2@gmail.com>
2022-03-06 12:29:54 +0100

    fix bug

--- client/src/Components/ShippingData/Shipments.js
+++ client/src/Components/ShippingData/Shipments.js
@@ -39,9 +39,9 @@ const Shippings = () => {
             <li className='collection-header'>
                 <h4 className='center'>Shipping Logs</h4>
             </li>
-            {!loading && shippings.length === null ? (<p className='center'>No shipments to show..</p>) : (
+            {!loading && shippings !== null ? (
           shippings.slice(0, rowsPerPage).map((shipments, index) => <ShippingItem shipments={shipments} key={index} />)
-        )}
+        ) : (<p className='center'>No shipments to show..</p>) }
         </ul>
 
         <TablePagination
